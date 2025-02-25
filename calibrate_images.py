881ed837ea9b26ad30c1c230f1d2fccf1784288f
james-freeburn <jfreeburn@swin.edu.au>
2025-02-25 11:24:36 +1100

    Change FWHM calculation.

--- calibrate_images.py
+++ calibrate_images.py
@@ -3,12 +3,109 @@ import numpy as np
 import glob
 import os
 import argparse
-from astropy.wcs import WCS
+import subprocess
+from astroquery.vizier import Vizier
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+from astropy.wcs import WCS 
 from astropy.io import fits
-from prep_phot_funcs.extract_sources import extract_sources
+from astropy.stats import sigma_clip
 from phot_funcs.correct_photometry import photom_correct
-from datastats.calculate_FWHM import calculate_FWHM
 from astropy.modeling.functional_models import Moffat2D
+
+def cross_match(obs_cat, viz_cat):
+    radius_threshold = 2*u.arcsec
+
+    coords_viz = SkyCoord(ra=viz_cat.RA, dec=viz_cat.DEC, unit='deg')
+    coords_obs = SkyCoord(ra=obs_cat.RA, dec=obs_cat.DEC, unit='deg')
+
+    idx, d2d, d3d = coords_viz.match_to_catalog_3d(coords_obs)
+
+    sep_constraint = d2d <= radius_threshold
+
+    # Get entries in cat_ref with a match
+    viz_matched = viz_cat[sep_constraint]
+
+    # Get matched entries in cat_sci
+    obs_matched = obs_cat.iloc[idx[sep_constraint]]
+
+    # re-index to match two dfs
+    viz_matched = viz_matched.reset_index(drop=True)
+    obs_matched = obs_matched.reset_index(drop=True)
+
+    return obs_matched, viz_matched
+
+def read_cat(file):
+    with open(file,'r') as f:
+        string = f.read()
+        sexcolumns = [header.split()[1] for header in np.append(string.split('#')[1:-1],string.split('#')[-1].split('\n')[0])]
+        f.close()
+                                     
+    cat = np.genfromtxt(file, unpack=True, filling_values=np.nan,
+                        invalid_raise=False)
+    cat = pd.DataFrame(data=np.transpose(cat), columns=sexcolumns)
+    return cat
+
+def query_gaia(ra,dec):
+    cat = 'I/355/gaiadr3'
+    # Create a SkyCoord object
+    coords = SkyCoord(ra=ra * u.deg,
+                      dec=dec * u.deg,
+                      frame='icrs')
+    # Select only those columns with relevant info for Scamp
+    columns_select = ['RA_ICRS', 'e_RA_ICRS', 'DE_ICRS', 'e_DE_ICRS',
+                        'Source', 'Gmag', 'e_Gmag', 'Plx']
+    # Vizier object
+    v = Vizier(columns=columns_select, row_limit=-1)
+    # Query Vizier
+    t_result = v.query_region(coords, width=18.*u.arcmin,
+                                height=9.*u.arcmin,
+                                catalog=cat)
+    # Check if the sources were found
+    if len(t_result[cat]) == 0:
+        return None
+    else:
+        return t_result[cat].to_pandas()
+
+def extract_sources(fitsfile, config_dir):
+    catfilename = fitsfile.replace('.fits', '.cat')
+    command =   f"sex -c {config_dir}/default.config "\
+                f"-CATALOG_NAME {catfilename} "\
+                f"-PARAMETERS_NAME {config_dir}/default.param -FILTER_NAME {config_dir}/default.conv "\
+                f"{fitsfile}"
+    subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+
+def get_fwhm(file,ccd,config):
+    with fits.open(file) as hdul:
+        ext = 0 if len(hdul) == 1 else 'S4'
+        hdu = hdul[ext]
+
+    extract_sources(file, args.config)
+
+    cat = read_cat(file.replace('.fits', '.cat'))
+    os.remove(file.replace('.fits', '.cat'))
+    cat.rename(columns={'X_WORLD':'RA', 'Y_WORLD':'DEC'}, inplace=True)
+    cat = cat[(cat['MAGERR_AUTO'] < 0.1) & (cat['ELLIPTICITY'] < 0.2)].reset_index(drop=True)
+
+    if os.path.exists(f"gaia_{ccd}.csv"):
+        gaia = pd.read_csv(f"gaia_{ccd}.csv")
+    else:
+        gaia = query_gaia(hdu.header['CRVAL1'], hdu.header['CRVAL2'])
+        gaia.rename(columns={'RA_ICRS':'RA','DE_ICRS':'DEC'}, inplace=True)
+        gaia = gaia[~np.isnan(gaia['Plx'])].reset_index(drop=True)
+        gaia.to_csv(f"gaia_{ccd}.csv",index=False)
+
+    cat_matched,gaia_matched = cross_match(cat, gaia)
+
+    FWHMs = np.array(cat_matched.FWHM_IMAGE)*0.263
+    FWHMs = FWHMs[~sigma_clip(FWHMs, sigma=2.0).mask]
+
+    with fits.open(file, mode='update') as hdul:
+        hdr = hdul[0].header
+        hdr['FWHM'] = (np.median(FWHMs), 'Median FWHM of sources in arcsec')
+        hdul.flush()
+
+    return np.median(FWHMs)
 
 def add_star(data,X,Y,flux,FWHM,alpha=4.765):
    
@@ -35,8 +132,6 @@ def add_star(data,X,Y,flux,FWHM,alpha=4.765):
                     data[j_index,i_index] += star[i,j]
 
     return data
-
-#LOOP THROUGH ALL FILES
 
 class phot_args:
     catalog = None
@@ -83,6 +178,10 @@ if __name__ == "__main__":
                         default='',
                         nargs=1,
                         help='Gaia catalog to perform parallax cuts.')
+    parser.add_argument('-s', '--config',
+                        type=str,
+                        default='config/',
+                        help='Path to SExtractor config directory.')
     args = parser.parse_args()
  
     field_run = args.field_run[0]
@@ -92,10 +191,8 @@ if __name__ == "__main__":
     phot_args.gaia = args.gaia
     phot_args.variables = args.variables
     phot_args.error_correction = False
-    print(args.datadir[0] + field_run + '/*/*/*' + ccd + '.fits')
     files = glob.glob(args.datadir[0] + field_run  + '/*/*/*' 
                       + ccd + '.fits')
-    print(files) 
     cals = []
     cal_std = []
     sextractor = 'sex'
@@ -105,11 +202,6 @@ if __name__ == "__main__":
     fluxes = np.geomspace(500,10000,5)
     
     for file in files:
-        print(file)
-        print(os.path.exists((args.datadir[0] + field_run + '/cals_' + ccd
-                           + 'temp/'
-                           + file.split('/')[-1]).replace(
-                               '.fits','_corr.csv')))
         if os.path.exists((args.datadir[0] + field_run + '/cals_' + ccd
                            + 'temp/'
                            + file.split('/')[-1]).replace(
@@ -132,11 +224,7 @@ if __name__ == "__main__":
             stdev = np.std(-2.5*np.log10(fluxes) - mags)
             cal_std.append(stdev)
             continue
-        fwhm=np.abs(calculate_FWHM(
-                    [file],
-                    sextractorloc=sextractor,
-                    verbose=False,
-                    quietmode=True)[0])
+        fwhm=get_fwhm(file,ccd,args.config)/0.263
     
         hdul = fits.open(file)
         data = hdul[0].data
@@ -156,9 +244,7 @@ if __name__ == "__main__":
         
         extract_sources(args.datadir[0] + field_run + '/cals_' + ccd 
                         + 'temp/' + 
-                        file.split('/')[-1], 
-                        args.datadir[0] + field_run + '/cals_' + ccd 
-                        + 'temp/',sextractor)
+                        file.split('/')[-1], args.config)
         phot_args.files = [(args.datadir[0] + field_run + '/cals_'
                             + ccd 
                             + 'temp/'
